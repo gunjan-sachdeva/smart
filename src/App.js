946580721e0a9ac9ea23f32ca57c3d1e4c2f4643
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2022-11-21 14:30:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
      <h1>we are doing version control system</h1> 
-      
+      <Home/>
       
       </div>
   );
