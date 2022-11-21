9a19e77c65558d5f5e6a6fff3f214710aea43a0e
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2022-11-21 14:27:39 +0530

    home

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Home extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Creating react App</h1>
+            </div>
+        )
+    }
+}
